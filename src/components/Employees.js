c2c0d48b75424ca6d923aba94db461e828b577dc
“[firstname <faith.gacheri@student.moringaschool.com>
2022-10-05 08:55:22 +0300

    add employees component

---
+++ src/components/Employees.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Employees(){
+    return (
+        <tr>
+          <td>{transaction.name}</td>
+          <td>{transaction.identity-no}</td>
+          <td>{transaction.gender}</td>
+          <td>{transaction.age}</td>
+        </tr>
+      );  
+}
+export default Employees;
--- src/components/EmployeesList.js
+++ src/components/EmployeesList.js
@@ -1,1 +1,27 @@
 import React from "react";
+
+function EmployeesList(){
+return (
+    <table className="ui celled striped padded table">
+      <tbody>
+        <tr>
+          <th>
+            <h3 className="ui center aligned header">Name</h3>
+          </th>
+          <th>
+            <h3 className="ui center aligned header">Identity-no</h3>
+          </th>
+          <th>
+            <h3 className="ui center aligned header">Gender</h3>
+          </th>
+          <th>
+            <h3 className="ui center aligned header">Age</h3>
+          </th>
+        </tr>
+  </tbody>
+  </table>
+);
+}
+
+export default EmployeesList;
+  
