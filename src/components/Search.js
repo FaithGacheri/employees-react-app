9d219308f9708614bf90d8121cc94709f8b4374a
“[firstname <faith.gacheri@student.moringaschool.com>
2022-10-05 09:13:22 +0300

    add search component

---
+++ src/components/Search.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Search(){
+    return (
+        <div className="ui large fluid icon input">
+          <input
+            type="text"
+            placeholder="Search your Employees Details"
+            // onChange={(e)=>handleSearch(e)}
+          />
+          <i className="circular search link icon"></i>
+        </div>
+      );
+    
+    }
+
+    export default Search;
